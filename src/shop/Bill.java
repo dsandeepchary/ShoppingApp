c86207e7eb21b29ee344886292d094d29b161d0b
Sandeep Chary <dsandeep.gandhichowk@gmail.com>
2016-10-20 22:15:41 +0530

    Bill.java updated
    Added conditions.

--- src/shop/Bill.java
+++ src/shop/Bill.java
@@ -16,40 +16,73 @@ public class Bill extends HttpServlet {
 		protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter out=response.getWriter();
 		HttpSession session=request.getSession();
-		response.setContentType("text/html");
 		Object bag=session.getAttribute("bags");
-		long o1=(Long)bag;
+		long o1;
+		if(bag==null)
+		{
+			o1=0;
+		}
+		else 
+			o1=(Long)bag;
 		Object clothe=session.getAttribute("clothes");
-		long o2=(Long)clothe;
+		long o2;
+		if(clothe==null)
+		{
+			o2=0;
+		}
+		else 
+			o2=(Long)clothe;
 		Object laptop=session.getAttribute("laptops");
-		long o3=(Long)laptop;
+		long o3;
+		if(laptop==null)
+		{
+			o3=0;
+		}
+		else 
+			o3=(Long)laptop;
 		Object mobile=session.getAttribute("mobiles");
-		long o4=(Long)mobile;
+		long o4;
+		if(mobile==null)
+		{
+			o4=0;
+		}
+		else 
+			o4=(Long)mobile;
 		Object shoe=session.getAttribute("shoes");
-		long o5=(Long)shoe;
+		long o5;
+		if(shoe==null)
+		{
+			o5=0;
+		}
+		else 
+			o5=(Long)shoe;
 		long total=o1+o2+o3+o4+o5;
 		out.println("<html>");
+		out.println("<center>");
 		out.println("<form action='invalidate'>");
 		out.println("<div align='right'><input type='submit' value='Logout'></div>");
 		out.println("</form>");
-		out.println("</center>");
 		out.println("<font color='blue'><h1>Your Total Bill</h1></font>");
 		out.println("<table bgcolor='orange' border='0'>");
-		out.println("<tr><td>Shoes Bill:</td>");
+		out.println("<tr><td>Shoes Bill  :</td>");
 		out.println("<td>"+shoe+"</td></tr>");
 		out.println("<tr><td>Clothes Bill:</td>");
 		out.println("<td>"+clothe+"</td></tr>");
 		out.println("<tr><td>Mobiles Bill:</td>");
 		out.println("<td>"+mobile+"</td></tr>");
-		out.println("<tr><td>Backpacks Bill:</td>");
+		out.println("<tr><td>Bags Bill   :</td>");
 		out.println("<td>"+bag+"</td></tr>");
 		out.println("<tr><td>Laptops Bill:</td>");
 		out.println("<td>"+laptop+"</td></tr>");
-		out.println("<tr colspan='2'>");
-		out.println("<td>Total Bill:"+total+"</td>");
+		out.println("<tr>");
+		out.println("<td>Total Bill      :</td>");
+		out.println("<td>"+total+"</td>");
 		out.println("</tr>");
 		out.println("</table>");
-		out.println("<h3><a href='index.html'>Shop More</a></h3>");
+		out.println("<h3><a href='welcome.html'>Shop More</a></h3>");
+		out.println("<form action='dummypay'>");
+		out.println("<div align='right'><input type='submit' value='Pay for the Bill'></div>");
+		out.println("</form>");	
 		out.println("</center></html>");
 		}
 }
